739d09167e11f4e66c3b8e393cebcedcab7e89e3
Nicole Giron <70114716+nicolegiron@users.noreply.github.com>
2021-11-02 22:41:10 -0400

    Add files via upload

---
+++ starter325.py
@@ -0,0 +1,18 @@
+# Recitation Activity #7
+# You must submit your file by the end of the allocated period for submissiom
+#
+# General Instructions
+#  - Do NOT change the function name
+#  - Replace  "ans1" with the passcode provided during your recitation. Do not remove the quotes
+#  - Enter the values in the corresponding bucket. Replace None with the correc value
+
+
+def answers():
+    # Activity answers
+
+    passcode = "aq947"
+
+    finalTable ={0:44, 1:12, 2:13, 3:16, 4:88, 5:23, 6:94, 7:39, 8:5, 9:11, 10:20}
+
+    # Do not modify the return statement
+    return passcode, finalTable
